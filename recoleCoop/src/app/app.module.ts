9c19a05c7c0c28bc7fe51f3d08cfbf2466961f62
juangrela96 <grela_96@hotmail.com>
2021-05-25 17:09:00 -0300

    Formulario para realizar pedido

--- recoleCoop/src/app/app.module.ts
+++ recoleCoop/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PedidoFormComponent } from './components/pedido-form/pedido-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PedidoFormComponent,
+    
   ],
   imports: [
     BrowserModule,

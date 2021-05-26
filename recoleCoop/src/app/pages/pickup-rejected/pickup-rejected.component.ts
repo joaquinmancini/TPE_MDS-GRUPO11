664c7b1d15f274121f29729108e4c80aedf11c63
joaquinmancini <andrx_it@hotmail.com>
2021-05-25 21:27:52 -0300

    TMG-50

--- recoleCoop/src/app/pages/pickup-rejected/pickup-rejected.component.ts
+++ recoleCoop/src/app/pages/pickup-rejected/pickup-rejected.component.ts
@@ -13,6 +13,8 @@ export class PickupRejectedComponent implements OnInit {
   imageRejectedPath = 'assets/img/pickup-rejected.png';
   imageAcopioPath = 'assets/img/acopio-map.png';
   pickupRejectedTitle = 'Lo sentimos...\nSu pedido de retiro no podrá ser realizado.\nUsted se encuentra a mas de 6km del centro de acopio.';
+  acopioAdressTitle = 'Acerque el material a \nnuestro centro ubicado \nen Darragueira 1380';
+  
   constructor() {}
 
   ngOnInit(): void {

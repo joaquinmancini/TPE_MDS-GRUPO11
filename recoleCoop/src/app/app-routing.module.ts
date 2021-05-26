04685c0dda87200dea270bd55e5de23f91dd2f94
joaquinmancini <andrx_it@hotmail.com>
2021-05-25 21:03:24 -0300

    TMG-49

--- recoleCoop/src/app/app-routing.module.ts
+++ recoleCoop/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { PickupRejectedComponent } from './pages/pickup-rejected/pickup-rejected.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
+  { path: 'pedido-rechazado', component: PickupRejectedComponent},
   { path: '', redirectTo: 'login',pathMatch: 'full'},
   { path: '**', redirectTo: 'login', pathMatch: 'full'},
 ];
--- recoleCoop/src/app/pages/pickup-rejected/pickup-rejected.component.ts
+++ recoleCoop/src/app/pages/pickup-rejected/pickup-rejected.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-pickup-rejected',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PickupRejectedComponent implements OnInit {
 
-  constructor() { }
+  imageRejectedPath = 'assets/img/pickup-rejected.png';
+  imageAcopioPath = 'assets/img/acopio-map.png';
+  pickupRejectedTitle = 'Lo sentimos...\nSu pedido de retiro no podrá ser realizado.\nUsted se encuentra a mas de 6km del centro de acopio.';
+  constructor() {}
 
   ngOnInit(): void {
+
   }
 
 }

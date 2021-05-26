fdcee242187b9775e583736a0e79a22bbe9d820f
Juan Martin Varela <varelajumartin@gmail.com>
2021-05-26 19:53:16 -0300

    Se agregan componentes header y footer

--- recoleCoop/src/app/app.module.ts
+++ recoleCoop/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

aedffd9fcffeff02d526ab3f008fb5ee2775c5e2
joaquinmancini <andrx_it@hotmail.com>
2021-06-16 18:04:45 -0300

    creacion componente manage-material

---
+++ recoleCoop/src/app/components/manage-materials/manage-materials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-materials',
+  templateUrl: './manage-materials.component.html',
+  styleUrls: ['./manage-materials.component.scss']
+})
+export class ManageMaterialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
